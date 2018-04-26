02efad941a972b68bf79bf807d248a974ee562bd
DOMAIN\rmason <rmason@petsathome.co.uk>
2018-04-26 16:36:20 +0100

    Add ftp vars to File endpoint and teststep properties classes

--- irontest-core-server/src/main/java/io/irontest/models/endpoint/FILEEndpointProperties.java
+++ irontest-core-server/src/main/java/io/irontest/models/endpoint/FILEEndpointProperties.java
@@ -6,9 +6,15 @@ import io.irontest.models.Properties;
  * Created by Zheng on 29/07/2017.
  */
 public class FILEEndpointProperties extends Properties {
+	private FileDestinationType destinationType = FileDestinationType.LOCAL_NETWORK;		// LOCAL_NETWORK is the default file transfer mode
+	private FileTransferProtocol transferProtocol = FileTransferProtocol.FTP;		// FTP is the default transfer protocol mode
 	private String fileName;
     private String filePath;
     private String contents;
+	private String hostname;
+	private String port;
+	private String username;
+	private String password;
     
     public String getFileName() {
 		return fileName;
@@ -32,5 +38,53 @@ public class FILEEndpointProperties extends Properties {
     
     public void setContents(String contents) {
     	this.contents = contents;
+    }
+    
+    public FileDestinationType getDestinationType() {
+    	return destinationType;
+    }
+    
+    public void setDestinationType(FileDestinationType destinationType) {
+    	this.destinationType = destinationType;
+    }
+    
+    public FileTransferProtocol getTransferProtocol() {
+    	return transferProtocol;
+    }
+    
+    public void setTransferProtocol(FileTransferProtocol transferProtocol) {
+    	this.transferProtocol = transferProtocol;
+    }
+    
+    public String getHostname() {
+    	return hostname;
+    }
+    
+    public void setHostname(String hostname) {
+    	this.hostname = hostname;
+    }
+    
+    public String getPort() {
+    	return port;
+    }
+    
+    public void setPort(String port) {
+    	this.port = port;
+    }
+    
+    public String getUsername() {
+    	return username;
+    }
+    
+    public void setUsername(String username) {
+    	this.username = username;
+    }
+    
+    public String getPassword() {
+    	return password;
+    }
+    
+    public void setPassword(String password) {
+    	this.password = password;
     }
 }
--- irontest-core-server/src/main/java/io/irontest/models/teststep/FILETeststepProperties.java
+++ irontest-core-server/src/main/java/io/irontest/models/teststep/FILETeststepProperties.java
@@ -9,6 +9,12 @@ public class FILETeststepProperties extends Properties {
 	private String fileName;
 	private String filePath;
 	private String contents;
+	private String destinationType;
+	private String protocol;
+	private String hostname;
+	private String port;
+	private String username;
+	private String password;
     
     public String getFileName() {
         return fileName;
@@ -32,5 +38,53 @@ public class FILETeststepProperties extends Properties {
     
     public void setContents(String contents) {
     	this.contents = contents;
+    }
+    
+    public String getDestinationType() {
+    	return destinationType;
+    }
+    
+    public void setDestinationType(String destinationType) {
+    	this.destinationType = destinationType;
+    }
+    
+    public String getProtocol() {
+    	return protocol;
+    }
+    
+    public void setProtocol(String protocol) {
+    	this.protocol = protocol;
+    }
+    
+    public String getHostname() {
+    	return hostname;
+    }
+    
+    public void setHostname(String hostname) {
+    	this.hostname = hostname;
+    }
+    
+    public String getPort() {
+    	return port;
+    }
+    
+    public void setPort(String port) {
+    	this.port = port;
+    }
+    
+    public String getUsername() {
+    	return username;
+    }
+    
+    public void setUsername(String username) {
+    	this.username = username;
+    }
+    
+    public String getPassword() {
+    	return password;
+    }
+    
+    public void setPassword(String password) {
+    	this.password = password;
     }
 }
