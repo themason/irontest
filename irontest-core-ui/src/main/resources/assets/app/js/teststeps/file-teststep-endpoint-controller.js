d0cd4c6ddf8b17dc7518740c86c1f2900ad957a3
DOMAIN\rmason <rmason@petsathome.co.uk>
2018-04-26 16:35:14 +0100

    Reset endpoint props in file endpoint controller on dest type change

--- irontest-core-ui/src/main/resources/assets/app/js/teststeps/file-teststep-endpoint-controller.js
+++ irontest-core-ui/src/main/resources/assets/app/js/teststeps/file-teststep-endpoint-controller.js
@@ -5,8 +5,13 @@
 //    ng-include also creates a scope.
 angular.module('irontest').controller('FILETeststepEndpointController', ['$scope',
   function($scope) {
-    $scope.endpointTypeChanged = function(isValid) {
-      $scope.teststep.endpoint.otherProperties = {};
+    $scope.fileEndpointDestinationTypeChanged = function(isValid) {
+      //  clear properties for Client connection mode, to avoid saving unintended values
+      var endpointProperties = $scope.teststep.endpoint.otherProperties;
+      endpointProperties.hostname = null;
+      endpointProperties.port = null;
+      endpointProperties.username = null;
+      endpointProperties.password = null;
 
       if (!$scope.isInShareEndpointMode()) {
         //  save test step immediately (so as to update endpoint)
